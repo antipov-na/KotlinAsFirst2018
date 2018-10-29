9285c7e0a9ee262e531b9d83a5c6a9f5df165caf
antipov-na <niki0911@rambler.ru>
2018-10-29 12:21:42 +0300

    сделал задание на квадраты в уроке 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -166,7 +166,10 @@ fun isCoPrime(m: Int, n: Int): Boolean {
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    if (m == 1 || n == 1) return true
+    return sqr(ceil(sqrt(m.toDouble()))) <= n
+}
 /**
  * Средняя
  *
